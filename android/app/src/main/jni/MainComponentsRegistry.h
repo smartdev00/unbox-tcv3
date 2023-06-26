73cff05ed809c54602c8718d50e78cdd57103d41
tonyrop00 <tonyrop00@gmail.com>
2023-06-25 16:28:53 -1200

    change package name

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/be.fostplus.theclick.development/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/be.fostplus.theclick/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
